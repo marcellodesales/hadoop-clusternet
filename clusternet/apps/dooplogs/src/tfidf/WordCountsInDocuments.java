36e4532bc9ac4911a9e1db57f05b33c4189c309e
Marcello de Sales <marcello.desales@gmail.com>
2010-12-31 20:41:38 +0000

    This revision refacts the TF-IDF example application by moving the Mapper and Reducer classes from the separate java artifacts to the drivers of each job.
    * WordFrequenceInDocument.java
    - Implements the first step of the algorithm by calculating the terms frequence.
    
    * WordCountsInDocuments.java
    - Implements the second step of the algorithm by calculating the word counts in each document.
    
    * WordsInCorpusTFIDF.java
    - Implements the third step of the algorithm by calculating the total number of words in the corpus and the tf and idf values,
    to finally calculate tf * idf.
    
    * *Mapper
    * *Reducer
    - Moving the mapper and reducer classes from the artifacts to each of the associated driver above.
    
    
    git-svn-id: svn://localhost/hadoop-clusternet/trunk@36 eff2079c-f815-4a70-be48-49cc87109067

--- clusternet/apps/dooplogs/src/tfidf/WordCountsInDocuments.java
+++ clusternet/apps/dooplogs/src/tfidf/WordCountsInDocuments.java
@@ -1,12 +1,17 @@
-// (c) Copyright 2009 Cloudera, Inc.
-// Hadoop 0.20.1 API Updated by Marcello de Sales (marcello.desales@gmail.com)
 package tfidf;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
@@ -15,6 +20,7 @@ import org.apache.hadoop.util.ToolRunner;
 /**
  * WordCountsInDocuments counts the total number of words in each document and 
  * produces data with the relative and total number of words for each document.
+ * 
  * @author Marcello de Sales (mdesales)
  */
 public class WordCountsInDocuments extends Configured implements Tool {
@@ -24,6 +30,69 @@ public class WordCountsInDocuments extends Configured implements Tool {
 
     // where to read the data from.
     private static final String INPUT_PATH = "1-word-freq";
+
+    public static class WordCountsForDocsMapper extends Mapper<LongWritable, Text, Text, Text> {
+
+        private Text docName = new Text();
+        private Text wordAndCount = new Text();
+
+        public WordCountsForDocsMapper() {
+        }
+
+        /**
+         * @param key is the byte offset of the current line in the file;
+         * @param value is the line from the file
+         * @param context
+         * 
+         *     PRE-CONDITION: aa@leornardo-davinci-all.txt    1
+         *                    aaron@all-shakespeare   98
+         *                    ab@leornardo-davinci-all.txt    3
+         * 
+         *     POST-CONDITION: Output <"all-shakespeare", "aaron=98"> pairs
+         */
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            String[] wordAndDocCounter = value.toString().split("\t");
+            String[] wordAndDoc = wordAndDocCounter[0].split("@");
+            this.docName.set(wordAndDoc[1]);
+            this.wordAndCount.set(wordAndDoc[0] + "=" + wordAndDocCounter[1]);
+            context.write(this.docName, this.wordAndCount);
+        }
+    }
+
+    public static class WordCountsForDocsReducer extends Reducer<Text, Text, Text, Text> {
+
+        private Text wordAtDoc = new Text();
+        private Text wordAvar = new Text();
+
+        public WordCountsForDocsReducer() {
+        }
+
+        /**
+         * @param key is the key of the mapper
+         * @param values are all the values aggregated during the mapping phase
+         * @param context contains the context of the job run 
+         * 
+         *        PRE-CONDITION: receive a list of <document, ["word=n", "word-b=x"]>
+         *            pairs <"a.txt", ["word1=3", "word2=5", "word3=5"]> 
+         *            
+         *       POST-CONDITION: <"word1@a.txt, 3/13">,
+         *            <"word2@a.txt, 5/13">
+         */
+        protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            int sumOfWordsInDocument = 0;
+            Map<String, Integer> tempCounter = new HashMap<String, Integer>();
+            for (Text val : values) {
+                String[] wordCounter = val.toString().split("=");
+                tempCounter.put(wordCounter[0], Integer.valueOf(wordCounter[1]));
+                sumOfWordsInDocument += Integer.parseInt(val.toString().split("=")[1]);
+            }
+            for (String wordKey : tempCounter.keySet()) {
+                this.wordAtDoc.set(wordKey + "@" + key.toString());
+                this.wordAvar.set(tempCounter.get(wordKey) + "/" + sumOfWordsInDocument);
+                context.write(this.wordAtDoc, this.wordAvar);
+            }
+        }
+    }
 
     public int run(String[] args) throws Exception {
 
